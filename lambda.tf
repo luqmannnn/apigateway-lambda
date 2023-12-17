735d56270381aea425cf4b3252f5cc968b5a95ef
iuolxvpq <luqmannurhakimt@gmail.com>
2023-12-17 17:56:32 +0800

    Rename file dir

--- lambda.tf
+++ lambda.tf
@@ -2,14 +2,14 @@
 
 data "archive_file" "lambda_one" {
   type        = "zip"
-  source_file = "./files/hello_lambda.js"
-  output_path = "./files/hello_lambda.zip"
+  source_file = "./lambda_function_code/hello_lambda.js"
+  output_path = "./lambda_function_code/hello_lambda.zip"
 }
 
 data "archive_file" "lambda_two" {
   type        = "zip"
-  source_file = "./files/congrats_lambda.js"
-  output_path = "./files/congrats_lambda.zip"
+  source_file = "./lambda_function_code/congrats_lambda.js"
+  output_path = "./lambda_function_code/congrats_lambda.zip"
 }
 
 resource "aws_lambda_function" "lambda_one" {
