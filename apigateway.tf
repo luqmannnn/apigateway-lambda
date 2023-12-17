931f28119efae50dc54808a3e2a620b58a507fce
iuolxvpq <luqmannurhakimt@gmail.com>
2023-12-18 01:22:01 +0800

    Remove api gateway

--- apigateway.tf
+++ apigateway.tf
@@ -1,41 +1,41 @@
-# API Gateway
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name = "LambdaAPIGateway"
-}
+# # API Gateway
+# resource "aws_api_gateway_rest_api" "api_gateway" {
+#   name = "LambdaAPIGateway"
+# }
 
-resource "aws_api_gateway_resource" "api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "lambda"
-}
+# resource "aws_api_gateway_resource" "api_resource" {
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+#   path_part   = "lambda"
+# }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "method" {
+#   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id   = aws_api_gateway_resource.api_resource.id
+#   http_method   = "GET"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_integration" "hello_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "GET"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_one.invoke_arn
-}
+# resource "aws_api_gateway_integration" "hello_integration" {
+#   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id             = aws_api_gateway_resource.api_resource.id
+#   http_method             = aws_api_gateway_method.method.http_method
+#   integration_http_method = "GET"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.lambda_one.invoke_arn
+# }
 
-resource "aws_api_gateway_integration" "congrats_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_two.invoke_arn
-}
+# resource "aws_api_gateway_integration" "congrats_integration" {
+#   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id             = aws_api_gateway_resource.api_resource.id
+#   http_method             = aws_api_gateway_method.method.http_method
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.lambda_two.invoke_arn
+# }
 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.hello_integration, aws_api_gateway_integration.congrats_integration]
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = "dev"
-}
+# resource "aws_api_gateway_deployment" "deployment" {
+#   depends_on = [aws_api_gateway_integration.hello_integration, aws_api_gateway_integration.congrats_integration]
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   stage_name  = "dev"
+# }
