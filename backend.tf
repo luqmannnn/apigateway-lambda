5e3205dd0a31f3e5ddf70ba214e214ba2b33cee6
iuolxvpq <luqmannurhakimt@gmail.com>
2023-12-17 17:54:10 +0800

    different bucket

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce5-tfstate-bucket"
+    bucket = "sctp-ce5-tfstate-bucket-1"
     key    = "luqman-create-lambda-apigateway.tfstate" # Change the value of this to yourname-tf-workspace-act.tfstate for  example
     region = "us-east-1"
   }
